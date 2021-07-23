05cc23fc6be8d3770bf86d55ed4efccc98d12d5d
DLQ <1114626450@qq.com>
2021-07-23 12:23:13 +0800

    初始化工程项目结构

---
+++ jd-product/src/main/java/com/dlq/jdmall/product/ProductApplication.java
@@ -0,0 +1,17 @@
+package com.dlq.jdmall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *@program: jdmall
+ *@description:
+ *@author: Hasee
+ *@create: 2021-07-22 23:57
+ */
+@SpringBootApplication
+public class ProductApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+}
