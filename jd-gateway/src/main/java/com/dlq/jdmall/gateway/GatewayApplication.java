4c088a6c45e275a0c0a1053aa008bce1ba29ca9a
DLQ <1114626450@qq.com>
2021-07-24 21:40:24 +0800

    创建Gateway网关微服务

---
+++ jd-gateway/src/main/java/com/dlq/jdmall/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.dlq.jdmall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ *@program: jdmall
+ *@description:
+ *@author: Hasee
+ *@create: 2021-07-24 21:10
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
