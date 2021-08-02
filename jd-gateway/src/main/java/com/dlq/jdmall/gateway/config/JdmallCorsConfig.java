c1ac2e80279718721ee3ff49f484d99fc5c810b5
DLQ <1114626450@qq.com>
2021-08-02 23:20:51 +0800

    添加网关统一配置跨域--配置类JdmallCorsConfig

---
+++ jd-gateway/src/main/java/com/dlq/jdmall/gateway/config/JdmallCorsConfig.java
@@ -0,0 +1,32 @@
+package com.dlq.jdmall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @program: learn_parent
+ * @description: 网关跨域配置类
+ * @author: Hasee
+ * @create: 2020-07-07 20:48
+ */
+@Configuration
+public class JdmallCorsConfig {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); //是否允许携带cookie
+        config.addAllowedOrigin("*"); //可接受的域，是一个具体域名或者*（代表任意域名）
+        config.addAllowedHeader("*"); //允许携带的头
+        config.addAllowedMethod("*"); //允许访问的方式
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
